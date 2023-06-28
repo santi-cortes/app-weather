112e5bf95ea4c13add38309b6d76048d31dde797
Santi_Cortes <santya.2003.06@gmail.com>
2023-06-27 23:57:50 -0500

    fill page doc

--- pages/Doc.js
+++ pages/Doc.js
@@ -1,10 +1,10 @@
 import Footer from '@/components/Footer';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Doc = () => {
 	return (
 		<>
-			<div className="h-screen bg-white dark:bg-slate-950 dark:text-white text-slate-950 flex flex-col gap-2">
+			<div className="h-screen overflow-auto bg-white dark:bg-slate-950 dark:text-white text-slate-950 flex flex-col gap-2">
 				<div className="m-4 flex flex-col gap-4">
 					<h1 className="text-center">HOLA, BIENVENIDO A LA DOCUMENTACION!</h1>
 					<p>
@@ -22,8 +22,39 @@ const Doc = () => {
 						<li className="hover:underline cursor-pointer">Mejoras</li>
 					</ul>
 				</nav>
+				<section className="m-4 flex flex-col gap-4">
+					<h1 className="text-2xl">Inicio</h1>
+					<p>
+						La API meteorológica de OpenWeatherMap proporciona acceso a diversos
+						datos:
+					</p>
+					<ul className="m-4">
+						<li>Clima actual</li>
+						<li> Pronóstico de minutos para 1 hora</li>
+						<li> Pronóstico por hora para 48 horas</li>
+						<li>Pronóstico diario parar 8 días</li>
+						<li>Alertas metereológicas gubernamentales</li>
+						<li>
+							Datos metereológicos históricos para cualquier marca de tiempo
+							durante más de 40 años archivo histórico
+						</li>
+						<li>
+							Historial de datos metereologicos agregados diarios durante más de
+							40 años archivo histórico
+						</li>
+					</ul>
+				</section>
+				<section className="m-4 flex flex-col gap-4">
+					<h1 className="text-2xl">Endpoints usados</h1>
+				</section>
+				<section className="m-4 flex flex-col gap-4">
+					<h1 className="text-2xl">Mejoras</h1>
+				</section>
+				<section className="m-4 flex flex-col gap-4">
+					<h1 className="text-2xl">Sugerencias</h1>
+				</section>
+				<Footer />
 			</div>
-			<Footer />
 		</>
 	);
 };
